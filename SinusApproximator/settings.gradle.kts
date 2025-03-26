cceb657204b73e0268242c6707825ef63c4f23cf
Michal Harakal <michal.harakal@googlemail.com>
2025-03-26 23:50:49 +0100

    Fix typo

--- SinusApproximator/settings.gradle.kts
+++ SinusApproximator/settings.gradle.kts
@@ -28,8 +28,8 @@ dependencyResolutionManagement {
         maven {
             url = uri("https://maven.pkg.github.com/sk-ai-net/skainet")
             credentials {
-                username = providers.gradleProperty("gpr.user").orElse(System.getenv("GITHUB_USERNAME")).get()
-                password = providers.gradleProperty("gpr.key").orElse(System.getenv("GITHUB_TOKEN")).get()
+                username = providers.gradleProperty("gpr.user").orElse(System.getenv("GITHUB_ACTOR")).get()
+                password = providers.gradleProperty("gpr.token").orElse(System.getenv("GITHUB_TOKEN")).get()
             }
         }
     }
